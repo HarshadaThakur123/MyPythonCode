e4d886cd8715606dd2c7d3cb9cbb912f1ae6d943
Harshada Thakur <harshada.thakur2017@gmail.com>
2025-08-30 22:35:50 +0530

    Add files via upload

---
+++ Printsum.py
@@ -0,0 +1,23 @@
+"""
+a = 200
+b = 5 
+sum = a - b
+print(sum) 
+"""
+
+"""a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+sum=a+b
+print("sum:",sum)"""
+
+"""a=int(input("Enter first number:"))
+print("area of squre:",a*a )
+print("area of squre:",a**2 )"""
+
+"""num1=float(input("Enter first number:"))
+num2=float(input("Enter first number:"))
+print("avg of two number",((num1+num2)/2))"""
+
+a=int(input("Enter first number:"))
+b=int(input("Enter first number:"))
+print(a>=b)
