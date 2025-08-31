6153350e06ba6ec66d5f20fad737aa67621d602f
Harshada Thakur <harshada.thakur2017@gmail.com>
2025-08-31 22:00:01 +0530

    Add files via upload

---
+++ TypeConversion.py
@@ -0,0 +1,20 @@
+#Type Conversion 
+"""
+a=2 #int
+b=4.25 #float
+sum = a+b
+print(sum)
+"""
+
+#Type Casting
+"""
+a,b=1,"2"
+c=int(b)
+sum=a+c
+print(sum)
+"""
+a=float("2")  #string not work in type casting at the time of a+b
+b=4.25 
+sum = a+b
+print(type(a))
+print(sum)
