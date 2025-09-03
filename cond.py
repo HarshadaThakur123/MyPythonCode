874a16387e7c7acca42b765d0fed3aeb45bfc36d
Harshada Thakur <harshada.thakur2017@gmail.com>
2025-09-03 18:33:08 +0530

    Add files via upload

---
+++ cond.py
@@ -0,0 +1,18 @@
+age=21
+if (age>=18):
+    print("can vote and apply for licence")
+    print("can drive")
+
+num=5
+
+if(num>2):
+    print("greter than 2") #always cheack
+if(num>3):
+    print("greter than 3")
+
+num=10
+if(num>2):
+    print("greter than 2") #indentation (proper space)
+elif(num>5):
+    print("greter than 5") #if if condition false then cheack
+
